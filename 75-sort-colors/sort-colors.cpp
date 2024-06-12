96a70124ff207a26f46f5d073d67803cef9d345c
Salman Rayeen <92143099+mr-rayeen@users.noreply.github.com>
2024-06-12 10:02:33 +0530

    Added Comments to code

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //    Used Counting sort:
         map<int, int> mp;
-
+        //    Ye humko number of 0's, 1's, 2's. Dedega
         for(auto num : nums){
             mp[num]++;
         }
-        
+        //    Yaha map par iterate karenge jitni baar jo aya hoga utni baar usko 
+        //    nums me overwrite kar denge
         int i = 0;
         for(auto &num : mp){
             while(num.second--){
