1127480ba8efec53a8648a3e87b1cbddde9d9768
Salman Rayeen <92143099+mr-rayeen@users.noreply.github.com>
2024-07-20 21:54:58 +0530

    Time: 57 ms (21.69%) | Memory: 48.2 MB (95.31%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+        let i =n;
+    return function() {
+        return i++;
+
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
