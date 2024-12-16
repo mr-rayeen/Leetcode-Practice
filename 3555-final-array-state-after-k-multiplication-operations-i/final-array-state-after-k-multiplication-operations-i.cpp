694de3ed5e456edd150f788e40ba95bf5ec09469
Salman Rayeen <92143099+mr-rayeen@users.noreply.github.com>
2024-12-16 19:30:50 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (45.93%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -2,23 +2,11 @@ class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int n = nums.size();
-        priority_queue<pair<int, int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
 
-        for(int i = 0; i < n; i++){
-            pq.push({nums[i],i});
-        }
-
-        while(k--){
-            pair<int, int> x = pq.top();
-            int min = x.first;
-            min = min*multiplier;
-            pair<int, int> updated;
-            updated.first = min;
-            updated.second = x.second;
-            nums[updated.second] = updated.first;
-            pq.pop();
-            pq.push(updated);
-
+        for(int i = 0; i < k; i++){
+         // Find the minimum element in the array
+        auto minIt = min_element(nums.begin(), nums.end());
+        *minIt *= multiplier; // Update the minimum element directly
         }
 
         return nums;
