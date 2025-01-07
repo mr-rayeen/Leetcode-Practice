61339773cef72c920687d50233e067e1edca470f
Salman Rayeen <92143099+mr-rayeen@users.noreply.github.com>
2025-01-07 20:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (60.52%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,23 @@
+//Approach-1 (Brute Force)
+//T.C : O(m*n^2) , m = length of longest string in words, n = size of words
+//S.C : O(1)
+class Solution {
+public:
+    vector<string> stringMatching(vector<string> &words) {
+        vector<string> result;
+        int n = words.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                if (words[j].find(words[i]) != string::npos) {
+                    result.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
